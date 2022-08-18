5d533d40509eeb2e4baa7bd747625e258125889b
dianbing <jinmeng113@126.com>
2022-08-19 00:36:57 +0800

    Update Test.java
    add

--- src/test/java/com/isoldier/Test.java
+++ src/test/java/com/isoldier/Test.java
@@ -6,13 +6,9 @@ import java.util.Collections;
 public class Test {
 
 	public static void main(String[] args){
-		
-		int[] coins = {2,7,4,6};
-		Arrays.sort(coins);
-		System.out.println(Arrays.toString(coins));
 
-		Integer[] ints = new Integer[] { 3,5,2,9 };
-		Arrays.sort(ints,Collections.reverseOrder());
-		System.out.println(Arrays.toString(ints));
+		System.out.println(String.valueOf(0.072));
+		Double val = Double.parseDouble(String.valueOf(0.072)) - Double.parseDouble(String.valueOf(0.028)) / Double.parseDouble(String.valueOf(0.028));
+		System.out.println(val);
 	}
 }
